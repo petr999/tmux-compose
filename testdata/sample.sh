0e5a6b89d8cfc2f765060b101535dd1ebc171603
Peter Vereshagin <peter@vereshagin.org>
2022-07-31 13:57:36 +0400

    test: :speech_balloon: add every docker-compose service, give up bash trap SIGINT after attach

--- testdata/sample.sh
+++ testdata/sample.sh
@@ -5,6 +5,44 @@ cd ../../sh/dumbclicker
 tmux new -s dumbclicker-compose '
 	docker-compose up
 	bash -l
+' \; neww -n 'dumbclicker_nginx_1' '
+	PID=0;
+	try_next=1;
+	trap '\''
+		echo "trap pid: ${PID}"
+		kill -INT $PID
+		try_next=""
+	'\'' SIGINT
+	while [ '\''x1'\'' == "x${try_next}" ]; do 
+		bash -lc '\''
+			docker attach dumbclicker_nginx_1
+			sleep 1
+		'\'' &
+		PID=$!
+		echo "pid: ${PID}"
+		wait $PID
+	done
+	trap - SIGINT
+	bash -l
+' \; neww -n 'dumbclicker_h2o_1' '
+	PID=0;
+	try_next=1;
+	trap '\''
+		echo "trap pid: ${PID}"
+		kill -INT $PID
+		try_next=""
+	'\'' SIGINT
+	while [ '\''x1'\'' == "x${try_next}" ]; do 
+		bash -lc '\''
+			docker attach dumbclicker_h2o_1
+			sleep 1
+		'\'' &
+		PID=$!
+		echo "pid: ${PID}"
+		wait $PID
+	done
+	trap - SIGINT
+	bash -l
 ' \; neww -n 'dumbclicker_dumbclicker_1' '
 	PID=0;
 	try_next=1;
@@ -22,5 +60,6 @@ tmux new -s dumbclicker-compose '
 		echo "pid: ${PID}"
 		wait $PID
 	done
+	trap - SIGINT
 	bash -l
 '
